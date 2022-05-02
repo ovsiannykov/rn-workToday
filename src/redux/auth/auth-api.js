257b18e8e3b3ecede94c218d627f1b712f7c0d63
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-05-02 18:54:08 +0300

    settings employer

--- src/redux/auth/auth-api.js
+++ src/redux/auth/auth-api.js
@@ -1,10 +1,10 @@
 import instance from "../instance";
 
 export const authApi = {
-  registerStart: (body) => instance.post("/application/registerStart", body),
+  registerStart: (body) => instance.post("/emloyer/registerStart", body),
   registerSubmitCode: (body) =>
-    instance.post("/application/registerSubmitCode", body),
+    instance.post("/emloyer/registerSubmitCode", body),
   registerReSendCode: (body) =>
-    instance.post("/application/registerReSendCode", body),
-  auth: (body) => instance.post("/application/auth", body),
+    instance.post("/emloyer/registerReSendCode", body),
+  auth: (body) => instance.post("/emloyer/auth", body),
 };
