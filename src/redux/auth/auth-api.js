e8c0a107102f6263adfca54ce90ad444a7a079ab
Mike <mykhailo.ovsiannykov@isquad.dev>
2022-05-06 17:20:50 +0300

    Fix login bugs

--- src/redux/auth/auth-api.js
+++ src/redux/auth/auth-api.js
@@ -1,10 +1,19 @@
 import instance from "../instance";
 
+// export const authApi = {
+//   registerStart: (body) => instance.post("/emloyer/registerStart", body),
+//   registerSubmitCode: (body) =>
+//     instance.post("/emloyer/registerSubmitCode", body),
+//   registerReSendCode: (body) =>
+//     instance.post("/emloyer/registerReSendCode", body),
+//   auth: (body) => instance.post("/emloyer/auth", body),
+// };
+
 export const authApi = {
-  registerStart: (body) => instance.post("/emloyer/registerStart", body),
+  registerStart: (body) => instance.post("/application/registerStart", body),
   registerSubmitCode: (body) =>
-    instance.post("/emloyer/registerSubmitCode", body),
+    instance.post("/application/registerSubmitCode", body),
   registerReSendCode: (body) =>
-    instance.post("/emloyer/registerReSendCode", body),
-  auth: (body) => instance.post("/emloyer/auth", body),
+    instance.post("/application/registerReSendCode", body),
+  auth: (body) => instance.post("/application/auth", body),
 };
